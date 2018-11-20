555a43fd6189e38b6929df90cbd6d124234754df
1049612128 <1049612128@qq.com>
2018-11-20 18:09:26 +0800

    finish 2018/11/20

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store'
+import { Button } from 'mint-ui'
+import Vuelazyload from 'vue-lazyload'
+import loading from './common/imgs/loading.gif'
 Vue.config.productionTip = false
-
+// 注册全局组件标签
+Vue.component(Button.name, Button)
+Vue.use(Vuelazyload, {
+  loading
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/pages/Login/Login.vue'
 import Order from '@/pages/Order/Order.vue'
-
+import MSite from '@/pages/MSite/MSite.vue'
+import Search from '@/pages/Search/Search.vue'
+import Profile from '@/pages/Profile/Profile.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +17,18 @@ export default new Router({
       path: '/order',
       name: 'order',
       component: Order
+    }, {
+      path: '/msite',
+      name: 'MSite',
+      component: MSite
+    }, {
+      path: '/search',
+      name: 'Search',
+      component: Search
+    }, {
+      path: '/Profile',
+      name: 'Profile',
+      component: Profile
     }
   ]
 })
